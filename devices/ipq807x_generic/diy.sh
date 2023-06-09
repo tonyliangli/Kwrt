8f44acee690685b5a491891532efe71b45e7d45c
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-06-10 00:55:24 +0800

    update

--- devices/ipq807x_generic/diy.sh
+++ devices/ipq807x_generic/diy.sh
@@ -7,10 +7,6 @@ svn co https://github.com/robimarko/nss-packages/trunk/qca/qca-ssdk-shell packag
 
 sh -c "curl -sfL https://github.com/robimarko/openwrt/commit/23fa931934151f72c1655ffa62ff1a979575f07e.patch | patch -d './' -p1 --forward"
 
-sh -c "curl -sfL https://patch-diff.githubusercontent.com/raw/immortalwrt/immortalwrt/pull/859.patch | git apply -p1"
-
-git apply patches/*.bin.patch
-
 sed -i '/rm -rf $(KDIR)\/tmp/d' include/image.mk
 
 rm -rf feeds/kiddin9/{rtl8821cu,rtl88x2bu} package/kernel/mt76
