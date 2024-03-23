4261d41bbc5dd105cbac3e81ba6bac485615fd2a
Tongliang Li <tonyliangli@163.com>
2024-03-23 22:42:23 +0800

    Update to 23.05.3

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -2,21 +2,6 @@
 #=================================================
 shopt -s extglob
 
-function git_clone_path() {
-          branch="$1" rurl="$2" localdir="gitemp" && shift 2
-          git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
-          if [ "$?" != 0 ]; then
-            echo "error on $rurl"
-            return 0
-          fi
-          cd $localdir
-          git sparse-checkout init --cone
-          git sparse-checkout set $@
-		  mv -n $@/* ../$@/ || cp -rf $@ ../$(dirname "$@")/
-          cd ..
-		  rm -rf gitemp
-          }
-
 sed -i '$a src-git kiddin9 https://github.com/tonyliangli/openwrt-packages.git;master' feeds.conf.default
 sed -i "/telephony/d" feeds.conf.default
 
